66da0c903c0790a5b2bdf5a51e1ec6973dcc4e1c
nimojs <imhadley@163.com>
2015-04-19 12:59:37 +0800

    修复 windows 路径兼容错误

--- index.js
+++ index.js
@@ -1,80 +1,103 @@
-var log = console.log
+/*
+    传入 gulp.watch(funciton(event){}) 中的 event（event.path） 、 查找起始字符串(search)、替换字符串(replace) 和 扩展名
+    扩展名非必填（大部分情况下会自动识别，例如：event.path = 'index.sass' 若 ext 参数为空，ext = 'css'）
+*/
+var path = require('path')
 var main = function (event, search, replace, ext) {
+    var log = function (msg) {
+        console.log(msg)
+    }
+    var srcFilename, distFilename, srcPath, srcDir, distPath, distDir
+    var rStringToRegExp, fStringToPrefixRegExp, fStringToSuffixRegExp, rDirname
+    search = path.join(search)
+    replace = path.join(replace)
+    log('\n')
+    log('search: ' + search)
+    log('replace: ' + replace)
+
+    log('event.path: ' + event.path)
+    // /Users/nimojs/Documents/code/demo/src/index.js
+
     /*
-    search "/src"
-    replace "/dist"
+      将字符串转换为正则所需的正则
+      . $ ^ { 等均需要被转换为 \. \$ \^ \ {
     */
-    var rStringToRegExp = /([.$^{[(|)*+?\/])/g
-    // .replace(rStringToRegExp,'\\$1'))
-    var fStringToPrefixRegExp = function (str) {
+    rStringToRegExp = /([.$^{[(|)*+?\/\\])/g
+    fStringToPrefixRegExp = function (str) {
         return new RegExp(
                 '^' + (
                         str.replace(rStringToRegExp, '\\$1')
                       )
                );
     }
-    var fStringToSuffixRegExp = function (str) {
+    fStringToSuffixRegExp = function (str) {
         return new RegExp(
                       (
                         str.replace(rStringToRegExp, '\\$1')
                       ) + '$'
                );
     }
+
     if (typeof search === 'string') {
-        search = fStringToPrefixRegExp(search) // src ==> \/src\/
+        search = fStringToPrefixRegExp(search) // src/ ==> \src\//
     }
-    
-    var srcFilename = event.path.replace(/.*?([^\/]+)$/, '$1')
-    //log('srcFilename: ' + srcFilename)
+    srcFilename = path.basename(event.path)
+    log('srcFilename: ' + srcFilename)
     // index.js
-    
-    if (/\.coffee$/i.test(srcFilename)) {
+
+    if (/\.(coffee|node)$/i.test(srcFilename)) {
         ext = 'js'
-    } else if (/\.(scss|sass|less)$/i.test(srcFilename)) {
+    }
+    else if (/\.(scss|sass|less)$/i.test(srcFilename)) {
         ext = 'css'
-    } else if (/\.(handlebars|hbs)$/i.test(srcFilename)) {
+    }
+    else if (/\.(handlebars|hbs)$/i.test(srcFilename)) {
         ext = 'js'
     }
 
     if (ext) {
-        distFilename = srcFilename.replace(/[^.]+$/, ext);
-    } else {
+        if (/\./.test(srcFilename)) {
+            distFilename = srcFilename.replace(/[^.]+$/, ext);
+        }
+        else{
+            distFilename = srcFilename + '.' + ext
+        }
+        
+    }
+    else {
         distFilename = srcFilename;
     }
+    log('distFilename: ' + distFilename)
 
-    var rDirname = fStringToPrefixRegExp( process.cwd() )
-    //log('rDirname: ' + rDirname)
+    rDirname = fStringToPrefixRegExp(process.cwd())
+    log('rDirname: ' + rDirname)
     // ^\/Documents\/code\/gulp-watch-path\/
-    
-    var srcPath = event.path.replace(rDirname, '')
-                            .replace(/^\//, '')
-    //log('srcPath: ' + srcPath)
+    srcPath = event.path.replace(rDirname, '')
+                            .replace(/^[\/\\]/, '')
+    log('srcPath: ' + srcPath)
     // src/js/log.js
 
-
-    var srcDir = srcPath.replace(fStringToSuffixRegExp(srcFilename), '')
-    //log('srcDir: ' + srcDir)
+    srcDir = srcPath.replace(fStringToSuffixRegExp(srcFilename), '')
+    log('srcDir: ' + srcDir)
     // src/js/
 
-    var distPath = srcPath.replace(search, replace)
-    //log('distPath: ' + distPath)
+    distPath = srcPath.replace(search, replace)
+    log('distPath: ' + distPath)
     // dist/js/log.js
-
     if (ext) {
         distPath = distPath.replace(/[^.]+$/, ext);
     }
-    var distDir = distPath.replace(fStringToSuffixRegExp(distFilename), '')
-    //log('distDir: ' + distDir)
+    distDir = distPath.replace(fStringToSuffixRegExp(distFilename), '')
+    log('distDir: ' + distDir)
     // dist/js/
-    
+    log('\n')
     return {
+        srcFilename: srcFilename,
+        distFilename: distFilename,
         srcPath: srcPath,
         srcDir: srcDir,
         distPath: distPath,
-        distDir: distDir,
-        srcFilename: srcFilename,
-        distFilename: distFilename
+        distDir: distDir
     }
 }
-
 module.exports = main
